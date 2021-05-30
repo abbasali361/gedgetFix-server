eb661739bd585353bda1b75e96ae498dde03d33e
Ariful Islalm <arifsujon.bd@gmail.com>
2021-05-30 20:02:18 +0600

    serviceRequest api created

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5555;
 
 app.use(cors());
 app.use(bodyParser.json());
-console.log(process.env.DB_USER);
+// console.log(process.env.DB_USER);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,10 +18,12 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v0opz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  console.log('connection err', err)
+  // console.log('connection err', err)
+
+  // add new service
   const serviceCollection = client.db("gadgetFix").collection("services");
   app.post('/addService', (req, res) => {
     const newService = req.body;
@@ -33,7 +35,7 @@ client.connect(err => {
     })
   })
   // perform actions on the collection object
-  console.log('database connected successfully');
+  // console.log('database connected successfully');
 
   app.get('/service', (req, res) => {
     serviceCollection.find()
@@ -42,11 +44,31 @@ client.connect(err => {
       console.log('from database ', items);
     })
   })
+
+  app.get('/serviceRequest', (req, res) => {
+    serviceRequestCollection.find()
+    .toArray((err, items) => {
+      res.send(items)
+      console.log('from database ', items);
+    })
+  })
+  
+  // booked new service
+  const serviceRequestCollection = client.db("gadgetFix").collection("serviceRequest");
+  app.post('/bookedService', (req, res) => {
+    const newServiceRequest = req.body;
+    console.log('booked new service', newServiceRequest);
+    serviceRequestCollection.insertOne(newServiceRequest)
+    .then(result => {
+      console.log('insert count', result.insertedCount);
+      res.send(result.insertedCount > 0)
+    })
+  })
   
   // client.close();
 });
 
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
